d49c996c502644c73e1bfb4336b6923fdb70c156
PvleTheEngineer <157322981+GWMolekoa@users.noreply.github.com>
2024-07-30 23:22:55 +0200

    added docstrings to views.py for myband app

--- myband/views.py
+++ myband/views.py
@@ -6,13 +6,14 @@ def home(request):
     Render the home page with specific bands.
 
     Retrieves bands 'The Beatles', 'Wu-Tang Clan', and 'The Temptations' from the database
-    or raises a 404 error if not found. Passes these bands as context to the 'home.html' template.
+    or raises a 404 error if any of them are not found. Passes these bands as context to 
+    the 'home.html' template.
 
     Args:
-    - request: HTTP request object.
+        request (HttpRequest): The HTTP request object.
 
     Returns:
-    - Rendered response for the 'home' page.
+        HttpResponse: The rendered response for the 'home' page.
     """
     # Retrieve specific bands or raise 404 if not found
     beatles = get_object_or_404(Myband, name='The Beatles')
@@ -27,19 +28,19 @@ def home(request):
     }
     return render(request, 'myband/home.html', context)
 
-
 def band_list(request):
     """
     Render a list of bands.
 
     Retrieves bands 'The Beatles', 'Wu-Tang Clan', and 'The Temptations' from the database
-    or raises a 404 error if not found. Passes these bands as context to the 'band_list.html' template.
+    or raises a 404 error if any of them are not found. Passes these bands as context to 
+    the 'band_list.html' template.
 
     Args:
-    - request: HTTP request object.
+        request (HttpRequest): The HTTP request object.
 
     Returns:
-    - Rendered response for the 'band_list' page.
+        HttpResponse: The rendered response for the 'band_list' page.
     """
     # Retrieve specific bands or raise 404 if not found
     beatles = get_object_or_404(Myband, name='The Beatles')
