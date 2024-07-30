7ccce58f23894dbab7266dd2991de6032bec4fcc
PvleTheEngineer <157322981+GWMolekoa@users.noreply.github.com>
2024-07-30 23:17:50 +0200

    added docstrings to apps.py for myband app

--- myband/apps.py
+++ myband/apps.py
@@ -1,6 +1,15 @@
 from django.apps import AppConfig
 
-
 class MybandConfig(AppConfig):
+    """
+    Configuration for the Myband application.
+
+    This class is used to configure the Myband application, specifying 
+    the default auto field and the application name.
+
+    Attributes:
+        default_auto_field (str): The default field type to use for auto-generated fields.
+        name (str): The name of the application, which is used to reference it within Django.
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'myband'
