c4c168d9744eb96da0b61d7b5d95479bf7f24d0d
PvleTheEngineer <157322981+GWMolekoa@users.noreply.github.com>
2024-07-30 23:39:45 +0200

    added comments to urls.py in project folder for both myband and user_auth apps

--- capstone/urls.py
+++ capstone/urls.py
@@ -1,11 +1,9 @@
-
 from django.contrib import admin
 from django.urls import path, include
 
-app_name = 'myband'
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('myband.urls')),
-    path('user_auth/', include('django.contrib.auth.urls')),
-    path('user_auth/', include('user_auth.urls')),
+    path('admin/', admin.site.urls),  # Admin site URLs
+    path('', include('myband.urls')),  # Include URLs from the 'myband' app
+    path('user_auth/', include('django.contrib.auth.urls')),  # Django's built-in authentication URLs
+    path('user_auth/', include('user_auth.urls')),  
 ]
