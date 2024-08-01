c4501fab61789a31c5418f48c16ed62cb107b526
PvleTheEngineer <157322981+GWMolekoa@users.noreply.github.com>
2024-08-01 21:33:37 +0200

    modified index.rst files to configure sphinx documentation index page

--- docs/conf.py
+++ docs/conf.py
@@ -1,10 +1,3 @@
-import os
-import sys
-import django
-sys.path.insert(0, os.path.abspath('..'))
-os.environ['DJANGO_SETTINGS_MODULE'] = 'capstone.settings'
-django.setup()
-
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
@@ -13,7 +6,7 @@ django.setup()
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'myband_capstone_consolidation'
+project = 'capstone_consolidation'
 copyright = '2024, Gaopalelwe Molekoa'
 author = 'Gaopalelwe Molekoa'
 release = '00.00.01'
