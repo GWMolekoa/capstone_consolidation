1a98c1f2b37b17d5b0c6a53296de4ae545bd0e9c
PvleTheEngineer <157322981+GWMolekoa@users.noreply.github.com>
2024-08-01 22:07:25 +0200

    modified myband/apps.py file by removing duplicate object descriptions in docstrings

--- myband/apps.py
+++ myband/apps.py
@@ -8,8 +8,8 @@ class MybandConfig(AppConfig):
     the default auto field and the application name.
 
     Attributes:
-        default_auto_field (str): The default field type to use for auto-generated fields.
-        name (str): The name of the application, which is used to reference it within Django.
+        default_auto_field (str): The default field type to use for auto-generated fields. :noindex:
+        name (str): The name of the application, which is used to reference it within Django. :noindex:
     """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'myband'
